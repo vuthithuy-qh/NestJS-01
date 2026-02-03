c4427658d3ace0878d61d0880c823f6708b2482b
vuthithuy-qh <vuthithuy.qc1984@gmail.com>
2026-02-03 10:26:49 +0700

    Fix KEY

--- src/providers/MailerSend.provider.ts
+++ src/providers/MailerSend.provider.ts
@@ -1,37 +1,36 @@
-// mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7;
+// // mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7;
 
-import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
+// import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
 
-const MAILER_SEND_API_KEY =
-  process.env.MAILER_SEND_API_KEY ||
-  'mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7';
+// const MAILER_SEND_API_KEY =
+//   process.env.MAILER_SEND_API_KEY;
 
-const ADMIN_SENDER_EMAIL =
-  process.env.ADMIN_SENDER_EMAIL || 'thuyvu@test-3m5jgroyyr0gdpyo.mlsender.net';
-const ADMIN_SENDER_NAME = process.env.ADMIN_SENDER_NAME || 'ThuyVu';
+// const ADMIN_SENDER_EMAIL =
+//   process.env.ADMIN_SENDER_EMAIL;
+// const ADMIN_SENDER_NAME = process.env.ADMIN_SENDER_NAME || 'ThuyVu';
 
-const mailersendInstance = new MailerSend({
-  apiKey: MAILER_SEND_API_KEY,
-});
+// const mailersendInstance = new MailerSend({
+//   apiKey: MAILER_SEND_API_KEY,
+// });
 
-//Tao bien sendFrom: nguoi gui email
+// //Tao bien sendFrom: nguoi gui email
 
-const sendFrom = new Sender(ADMIN_SENDER_EMAIL, ADMIN_SENDER_NAME);
-const sendEmail = async ({ to, toName, subject, html }) => {
-  try {
-    const recipients = [new Recipient(to, toName)];
+// const sendFrom = new Sender(ADMIN_SENDER_EMAIL, ADMIN_SENDER_NAME);
+// const sendEmail = async ({ to, toName, subject, html }) => {
+//   try {
+//     const recipients = [new Recipient(to, toName)];
 
-    const emailParams = new EmailParams()
-      .setFrom(sendFrom)
-      .setTo(recipients)
-      .setReplyTo(sendFrom)
-      .setSubject(subject)
-      .setHtml(html);
+//     const emailParams = new EmailParams()
+//       .setFrom(sendFrom)
+//       .setTo(recipients)
+//       .setReplyTo(sendFrom)
+//       .setSubject(subject)
+//       .setHtml(html);
 
-    const data = await mailersendInstance.email.send(emailParams);
-    return data;
-  } catch (error) {
-    console.error('Failed to send email', error);
-    throw error;
-  }
-};
+//     const data = await mailersendInstance.email.send(emailParams);
+//     return data;
+//   } catch (error) {
+//     console.error('Failed to send email', error);
+//     throw error;
+//   }
+// };
--- src/providers/MailerSendWithTemplate.ts
+++ src/providers/MailerSendWithTemplate.ts
@@ -1,47 +1,46 @@
-// mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7;
+// // mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7;
 
-import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
+// import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
 
-const MAILER_SEND_API_KEY =
-  process.env.MAILER_SEND_API_KEY ||
-  'mlsn.c05ff75bfd449226194d1ecaa53a2d0d74ad2a2ad366055dc8725286e388f8b7';
+// const MAILER_SEND_API_KEY =
+//   process.env.MAILER_SEND_API_KEY;
 
-const ADMIN_SENDER_EMAIL =
-  process.env.ADMIN_SENDER_EMAIL || 'thuyvu@test-3m5jgroyyr0gdpyo.mlsender.net';
-const ADMIN_SENDER_NAME = process.env.ADMIN_SENDER_NAME || 'ThuyVu';
+// const ADMIN_SENDER_EMAIL =
+//   process.env.ADMIN_SENDER_EMAIL;
+// const ADMIN_SENDER_NAME = process.env.ADMIN_SENDER_NAME || 'ThuyVu';
 
-const mailersendInstance = new MailerSend({
-  apiKey: MAILER_SEND_API_KEY,
-});
+// const mailersendInstance = new MailerSend({
+//   apiKey: MAILER_SEND_API_KEY,
+// });
 
-//Tao bien sendFrom: nguoi gui email
+// //Tao bien sendFrom: nguoi gui email
 
-const sendFrom = new Sender(ADMIN_SENDER_EMAIL, ADMIN_SENDER_NAME);
-const sendEmail = async ({
-  to,
-  toName,
-  subject,
-  html,
-  templateId,
-  personalizationData,
-}) => {
-  try {
-    const recipients = [new Recipient(to, toName)];
+// const sendFrom = new Sender(ADMIN_SENDER_EMAIL, ADMIN_SENDER_NAME);
+// const sendEmail = async ({
+//   to,
+//   toName,
+//   subject,
+//   html,
+//   templateId,
+//   personalizationData,
+// }) => {
+//   try {
+//     const recipients = [new Recipient(to, toName)];
 
-    const emailParams =
-      //   .setHtml(html);
-      new EmailParams()
-        .setFrom(sendFrom)
-        .setTo(recipients)
-        .setReplyTo(sendFrom)
-        .setSubject(subject)
-        .setTemplateId(templateId) // Template ID dc tao tu maildersend Dashboard
-        .setPersonalization(personalizationData); // dynamic data, cac bien truyen vao template
+//     const emailParams =
+//       //   .setHtml(html);
+//       new EmailParams()
+//         .setFrom(sendFrom)
+//         .setTo(recipients)
+//         .setReplyTo(sendFrom)
+//         .setSubject(subject)
+//         .setTemplateId(templateId) // Template ID dc tao tu maildersend Dashboard
+//         .setPersonalization(personalizationData); // dynamic data, cac bien truyen vao template
 
-    const data = await mailersendInstance.email.send(emailParams);
-    return data;
-  } catch (error) {
-    console.error('Failed to send email', error);
-    throw error;
-  }
-};
+//     const data = await mailersendInstance.email.send(emailParams);
+//     return data;
+//   } catch (error) {
+//     console.error('Failed to send email', error);
+//     throw error;
+//   }
+// };
